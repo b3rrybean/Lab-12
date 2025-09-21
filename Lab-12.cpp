4b01f35e2e53c97a02e56d6c5ec1f618b6967b7d
Nicole C. <nmcanson@gmail.com>
2025-09-21 00:31:18 -0700

    Third commit

--- Lab-12.cpp
+++ Lab-12.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <array>
+#include <algorithm>
+#include <numeric>
 using namespace std;
 
 const int SIZE = 30;
@@ -17,4 +19,21 @@ int main() {
 
     // Basic info
     cout << "Array size: " << temps.size() << endl;
+    cout << "First element: " << temps.front() << endl;
+    cout << "Last element: " << temps.back() << endl;
+    cout << "Element at index 5: " << temps.at(5) << endl;
+    cout << "Is empty? " << (temps.empty() ? "Yes" : "No") << endl;
+    cout << "Data address: " << temps.data() << endl;
+
+    // Sort ascending
+    sort(temps.begin(), temps.end());
+    cout << "Sorted temps: ";
+    for (int t : temps) cout << t << " ";
+    cout << endl;
+
+    // Sort descending
+    sort(temps.rbegin(), temps.rend());
+    cout << "Reverse sorted temps: ";
+    for (int t : temps) cout << t << " ";
+    cout << endl;
 }
